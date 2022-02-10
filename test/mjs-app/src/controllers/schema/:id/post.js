d55ad62d742c280d02acd9e2e2f33de02a4b3cd5
Sean Ferguson <sean.ferguson@truepill.com>
2022-02-10 14:33:58 -0800

    Add docs for validation and add niceties to controller format

--- test/mjs-app/src/controllers/schema/:id/post.js
+++ test/mjs-app/src/controllers/schema/:id/post.js
@@ -1,24 +1,26 @@
 export default {
-  params: {
-    type: 'object',
-    required: ['id'],
-    properties: {
-      id: { type: 'number', minimum: 100000 },
+  schemas: {
+    params: {
+      type: 'object',
+      required: ['id'],
+      properties: {
+        id: { type: 'number', minimum: 100000 },
+      },
     },
-  },
-  query: {
-    type: 'object',
-    required: ['middleName'],
-    properties: {
-      middleName: { type: 'string', minimum: 1 },
+    query: {
+      type: 'object',
+      required: ['middleName'],
+      properties: {
+        middleName: { type: 'string', minimum: 1 },
+      },
     },
-  },
-  body: {
-    type: 'object',
-    required: ['firstName', 'lastName'],
-    properties: {
-      firstName: { type: 'string', minimum: 1 },
-      lastName: { type: 'string', minimum: 1 },
+    body: {
+      type: 'object',
+      required: ['firstName', 'lastName'],
+      properties: {
+        firstName: { type: 'string', minimum: 1 },
+        lastName: { type: 'string', minimum: 1 },
+      },
     },
   },
   handler: (req) => req.validatedData,
